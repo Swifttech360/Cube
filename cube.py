a452e8ddcc29745928ed05c7170e60f7e201411b
Swifttech360 <71281505+Swifttech360@users.noreply.github.com>
2025-01-22 17:57:54 -0500

    Initial project creation

---
+++ cube.py
@@ -0,0 +1,7 @@
+#Write a program in cube.py that takes the length of an edge (an integer) as input and prints the cube’s surface area
+# as output. For this exercise, write a program that contains an introductory docstring. This documentation should
+# describe what the program will do (analysis) and how it will do it (design the program in the form of a pseudocode
+# algorithm). Include suitable prompts for all inputs, and label all outputs appropriately.
+# After you have coded a program, be sure to test it with a reasonable set of legitimate inputs.
+
+#
