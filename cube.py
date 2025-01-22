5a0a70149694d26918bfd0be8351e7ff877ac1a3
Swifttech360 <71281505+Swifttech360@users.noreply.github.com>
2025-01-22 18:42:30 -0500

    un documented finished program

--- cube.py
+++ cube.py
@@ -3,5 +3,19 @@
 # describe what the program will do (analysis) and how it will do it (design the program in the form of a pseudocode
 # algorithm). Include suitable prompts for all inputs, and label all outputs appropriately.
 # After you have coded a program, be sure to test it with a reasonable set of legitimate inputs.
-
-#
+loop = True
+while loop == True:
+	unfilteredSideLength = input("Enter cube side length in any unit to find the surface area, or type 'end' to end "
+	                             "the program:\n")
+	if unfilteredSideLength == "end":
+		break
+	try:
+		sideLength = float(unfilteredSideLength)
+	except:
+		Exception
+		print('Invalid input')
+		continue
+	surfaceArea = (sideLength ** 2 * 6)
+	surfaceArea = f"{surfaceArea:.0f}" if surfaceArea % 1 == 0 else surfaceArea
+	print(f'\n({unfilteredSideLength}²)6 = {surfaceArea} units\n')
+	
